20f33a06b5ee6a6bdee8fb302bf456c27fa04fd5
DanielElvis <mj.ravand1379@gmail.com>
2024-09-19 00:39:37 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import pytesseract
+from PIL import Image
+
+# Tesseract Config
+pytesseract.pytesseract.tesseract_cmd = r"c:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+# example
+
+def image_to_string(img):
+    image = Image.open(img)
+    response = pytesseract.image_to_string(image)
+    return f" Your Text Is : {response}"
+
+print(image_to_string("your image *"))
+
